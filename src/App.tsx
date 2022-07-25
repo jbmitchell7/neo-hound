c507ee8b5c7a55972256bce4c79e99efd4aa4b1c
Jake <jake@Jake-Laptop-2017.localdomain>
2022-07-25 11:14:29 -0500

    added api key env

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { fetchGet } from './api/nasa-neo';
 import './App.scss';
 
-function App() {
+const App = () => {
+  const [neo, setNeo] = useState('');
+
+  const getNeo = async () => {
+    const neoName = await fetchGet('/neo/3542519', {});
+    setNeo(neoName.name);
+  }
+
+  getNeo();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {neo}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/api/nasa-neo.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_API_KEY
+const API_URL = 'https://api.nasa.gov/neo/rest/v1';
+
+const neoFetch = async (url: String, method = 'GET', payload = {}) => {
+
+    let headers = {
+        'Content-Type': 'application/json;charset=utf-8',
+    }
+
+    try {
+
+        const options = {
+            headers,
+            method,
+            url: `${API_URL}${url}?api_key=${apiKey}`,
+            data: payload,
+        }
+
+        const response = await axios(options);
+
+        return response.data
+
+    } catch (err) {
+        throw new Error("Error retrieving data")
+    }
+}
+
+//export const fetchPost = async (url: String, payload: any) => neoFetch(url, 'POST', payload);
+export const fetchGet = async (url: String, payload: any) => neoFetch(url, 'GET', payload);
+//export const fetchPut = async (url: String, payload: any) => neoFetch(url, 'PUT', payload);
+//export const fetchDelete = async (url: String, payload: any) => neoFetch(url, 'DELETE', payload);
